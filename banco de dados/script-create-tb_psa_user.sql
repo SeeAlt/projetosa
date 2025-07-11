8ce236f992a6e518015f5e0a93e82aaa665547bd
SeeAlt <gdwgizeliwanzeler@gmail.com>
2025-07-11 11:20:24 -0300

    script-create-tb_psa_user.sql

--- banco de dados/script-create-tb_psa_user.sql
+++ banco de dados/script-create-tb_psa_user.sql
@@ -1,13 +1,12 @@
 CREATE TABLE tb_psa_user(
-	id_user INT NOT NULL AUTO_INCREMENT,
+    id_user INT NOT NULL AUTO_INCREMENT,
     user_name VARCHAR(255) NOT NULL,
-    user_passsword VARCHAR(100) NOT NULL,
+    user_password VARCHAR(100) NOT NULL,
     user_email VARCHAR(255) NOT NULL,
     user_cpf INT NOT NULL UNIQUE,
-    user_cep INT NOT NULL,
+    user_phone INT(11) NOT NULL,
     user_address VARCHAR(255) NOT NULL,
-    is_admin BIT NOT NULL,
+    is_admin TINYINT DEFAULT 0,
     
 	CONSTRAINT pk_user PRIMARY KEY(id_user)
     );
-    
