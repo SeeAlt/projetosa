87f6dc7b3d37c19f01581cfa4bf7e5898aeea3fd
SeeAlt <gdwgizeliwanzeler@gmail.com>
2025-07-11 11:18:02 -0300

    insert-user.php

--- insert-user.php
+++ insert-user.php
@@ -4,22 +4,42 @@
 
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-        $user_name = $_POST["name"];
-        $user_password = $_POST["password"];
-        $user_email = $_POST["email"];
-        $user_cep = $_POST["cep"];
-        $user_address = $_POST["address"];
-        $is_admin = $_POST["admin"];
+        try {
+            $username = $_POST["username"] ?? '';;
+            $userpassword = $_POST["userpassword"] ?? '';;
+            $useremail = $_POST["useremail"] ?? '';;
+            $usercpf = $_POST["usercpf"] ?? '';;
+            $userphone = $_POST["userphone"] ?? '';;
+            $useraddress = $_POST["useraddress"] ?? '';;
+            $isadmin = isset($_POST["isadmin"]) ? 1 : 0;
 
-        $sql = "INSERT INTO tb_psa_user(user_name, user_password, user_email, user_cep, user_address, is_admin)
-                VALUES (:name, :password, :email, :cep, :address, :admin)";
+            if (empty($username)) {
+                throw new Exception("Nome não pode ser vazio.");
+        }
+        
+        $sql = "INSERT INTO tb_psa_user (user_name, user_password, user_email, user_cpf, user_phone, user_address, is_admin)
+                VALUES (:username, :userpassword, :useremail, :usercpf, :userphone, :useraddress, :isadmin)";
         $stmt = $pdo->prepare($sql);
-        $stmt->bindParam(':name', $user_name);
-        $stmt->bindParam(':password', $user_passsword);
-        $stmt->bindParam(':email', $user_email);
-        $stmt->bindParam(':cep', $user_cep);
-        $stmt->bindParam(':address', $user_address);
-        $stmt->bindParam(':admin', $is_admin);
 
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':userpassword', $userpassword);
+        $stmt->bindParam(':useremail', $useremail);
+        $stmt->bindParam(':usercpf', $usercpf);
+        $stmt->bindParam(':userphone', $userphone);
+        $stmt->bindParam(':useraddress', $useraddress);
+        $stmt->bindParam(':isadmin', $isadmin);
 
+        $stmt->execute();
+
+        echo  "Usuário cadastrado com sucesso!";
+
+    } catch (PDOException $e) {
+        echo json_encode(["success" => false, "message" => "Erro ao cadastrar: " . $e->getMessage()]);
+    } catch (Exception $e) {
+        echo json_encode(["success" => false, "message" => "Erro: " . $e->getMessage()]);
     }
+} else {
+    echo json_encode(["success" => false, "message" => "Erro no envio do formulário."]);
+}
+exit;
+?>
